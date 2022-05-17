be279c062dcc4044b58ed085cdb010a5cf7ffbee
philhub85 <hudsonphilip2@gmail.com>
2022-05-17 15:54:45 -0400

    commit to master

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask
-def create_app():
-    app = Flask(__name__)
-    @app.route('/')
-    def hello_world():
-        return 'Hello, this is the app for 5/17/22!'
-    return app
-app = create_app()
+
+app = Flask("abc")
+
+@app.route('/')
+def hello_world():
+    return 'Hello,World!'
